0794dbc0e0979f6eb2499770d880e92184855274
Naresh K <nareshkaranavath1616@gmail.com>
2024-12-10 18:21:21 +0530

    extension of files

--- src/main/java/com/jwttoken/config/WebSecurityConfig.java
+++ src/main/java/com/jwttoken/config/WebSecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +25,19 @@ public class WebSecurityConfig {
                                           .formLogin(Customizer.withDefaults())
                                           .httpBasic(Customizer.withDefaults());
                              return httpSecurity.build();
+    }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails userDetails =
+                User.withUsername("naresh")
+                .password("{noop}Rainier@1995")
+                .roles("USER")
+                .build();
+       UserDetails userDetails1 =
+               User.withUsername("nikhle")
+               .password("{noop}nikhle")
+               .roles("USER")
+               .build();
+       return new InMemoryUserDetailsManager(userDetails, userDetails1);
     }
 }
